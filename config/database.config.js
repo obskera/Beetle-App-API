d58eeccfca2a6d502481825740414e734adaf8bc
BeetleReportingApp <BeetleReportingApp@gmail.com>
2022-06-24 23:48:08 -0700

    removed testing logs

--- config/database.config.js
+++ config/database.config.js
@@ -1,7 +1,3 @@
-// module.exports = {
-//    // url: 'mongodb://localhost:27017/beetle'
-// url: "mongodb+srv://beetle:BeetleBorgs@cluster0.kr3vz.mongodb.net/?retryWrites=true&w=majority"
-// }
 module.exports = {
    url: process.env.MONGOLAB_URI
 }
